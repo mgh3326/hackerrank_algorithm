1d71bfb8dfa4473b434ce91bb589e223665f3d98
robin <robin@watcha.com>
2022-07-24 00:17:49 +0900

    complete

---
+++ challenges/staircase/main.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(n):
+        value = n - i - 1
+        print(" " * value + "#" * (i + 1))
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
