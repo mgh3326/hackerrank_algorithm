0c35113e2c3a4bbe2b80a5ab919829a39a05c837
robin <robin@watcha.com>
2022-07-26 18:02:30 +0900

    complete

---
+++ challenges/challenges/grading/main.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    results = []
+    for grade in grades:
+        value = grade % 5
+        if grade < 38 or value == 0:
+            pass
+        elif value >= 3:
+            grade += 5 - value
+        results.append(grade)
+    return results
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ challenges/challenges/grading/test.py
@@ -0,0 +1,17 @@
+import os
+
+import main
+
+
+def test_solution():
+    file = open("./input1.txt", 'r')
+    input_strings = file.read().splitlines()
+    grades_count = input_strings[0]
+    input_strings = list(map(int, input_strings))
+    output_strings = file.read().splitlines()
+    grades = input_strings[1:]
+
+    file = open("./output1.txt", 'r')
+    output_strings = file.read().splitlines()
+    output_strings = list(map(int, output_strings))
+    assert main.gradingStudents(grades) == output_strings
